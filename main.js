b9d0d428defe2d6f4f1e7b2fa7e379dae6282752
Felipe Notebook <fecantareli@outlook.com>
2025-06-16 16:33:36 -0300

    Adaptação de projetos, e algumas partes do css

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import projetos from "./projects.js";
+
 window.addEventListener('DOMContentLoaded', () => {
   const buttonMenu = document.getElementById("btmenu");
   const listMenu = document.getElementById("menu_lista");
@@ -21,4 +23,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
   inicio();
   buttonMenu.onclick = inteMenu;
+
+  criarProjetos(projetos);
 });
+
+
+
+function criarProjetos(projetos){
+  const section = document.querySelector(".container__projetos")
+  const section_projects = document.querySelector(".projects");
+for(let i=0; i<projetos.length; i++){
+  const projeto = document.createElement("div");
+  projeto.classList.add("projetos")
+
+  const link = document.createElement("a");
+  link.href = projetos[i].href;
+
+  const image = document.createElement("img");
+  image.classList.add("projetos__imagem");
+  image.src = projetos[i].src;
+
+  const projeto_conhecimento= document.createElement("div");
+  projeto_conhecimento.classList.add("projetos__conhecimento")
+
+  const projeto_conhecimento_texto=document.createElement("div")
+  projeto_conhecimento_texto.classList.add("projetos__conhecimento__texto")
+
+  //Fazer com que crie sozinho os conhecimentos
+   projetos[i].conhecimentos.forEach(conhecimento => {
+      const icon = document.createElement("i");
+      icon.className = conhecimento;
+      icon.setAttribute("aria-label", conhecimento.split(" ")[1].toUpperCase());
+      projeto_conhecimento_texto.appendChild(icon);
+    });
+  
+projeto_conhecimento.appendChild(projeto_conhecimento_texto);
+
+link.appendChild(image);
+link.appendChild(projeto_conhecimento);
+
+projeto.appendChild(link);
+
+section_projects.appendChild(projeto);
+
+section.appendChild(section_projects);
+}
+}
+
---
+++ projects.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    src: "../assets/Port.png",
+    name: "Portifólio",
+    conhecimentos : ["fab fa-html5","fab fa-css3" ],
+    href:"https://github.com/FeCantareliA/Portifolio",
+  },
+  {
+    src: "../assets/Port.png",
+    name: "Portifólio",
+    conhecimentos : ["fab fa-html5","fab fa-css3" ],
+    href:"https://github.com/FeCantareliA/Portifolio",
+  },
+  {
+    src: "../assets/Port.png",
+    name: "Portifólio",
+    conhecimentos : ["fab fa-html5","fab fa-css3" ],
+    href:"https://github.com/FeCantareliA/Portifolio",
+  },
+  {
+    src: "../assets/Port.png",
+    name: "Portifólio",
+    conhecimentos : ["fab fa-html5","fab fa-css3" ],
+    href:"https://github.com/FeCantareliA/Portifolio",
+  },
+  
+];
