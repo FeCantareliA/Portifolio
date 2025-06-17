de402a255ba40b59f5491866b5b44bf99c02bb93
Felipe <fecantareli@outlook.com>
2025-06-17 10:57:34 -0300

    Atualização projetos

--- projects.js
+++ projects.js
@@ -1,27 +1,39 @@
 export default [
   {
-    src: "../assets/Port.png",
+    src: "../assets/projetos/Port.png",
     name: "Portifólio",
     conhecimentos : ["fab fa-html5","fab fa-css3" ],
     href:"https://github.com/FeCantareliA/Portifolio",
   },
   {
-    src: "../assets/Port.png",
-    name: "Portifólio",
-    conhecimentos : ["fab fa-html5","fab fa-css3" ],
-    href:"https://github.com/FeCantareliA/Portifolio",
+    src: "../assets/projetos/Supramercado.png",
+    name: "Supermercado",
+    conhecimentos : ["fab fa-java","fa-solid fa-database" ],
+    href:"https://github.com/FeCantareliA/Supramercado",
   },
   {
-    src: "../assets/Port.png",
-    name: "Portifólio",
-    conhecimentos : ["fab fa-html5","fab fa-css3" ],
-    href:"https://github.com/FeCantareliA/Portifolio",
+    src: "../assets/projetos/labirinto_1_image.png",
+    name: "Algoritmo A*",
+    conhecimentos : ["fab fa-python" ],
+    href:"https://github.com/FeCantareliA/Algoritmo-A-Estrela",
   },
   {
-    src: "../assets/Port.png",
-    name: "Portifólio",
-    conhecimentos : ["fab fa-html5","fab fa-css3" ],
-    href:"https://github.com/FeCantareliA/Portifolio",
+    src: "../assets/projetos/Romance.png",
+    name: "Romance",
+    conhecimentos : ["fab fa-html5","fab fa-css3","fa-brands fa-js" ],
+    href:"https://github.com/FeCantareliA/Romance",
+  },
+  {
+    src: "../assets/projetos/SGBD.png",
+    name: "SGBD",
+    conhecimentos : ["fab fa-java","fa-solid fa-database",],
+    href:"https://github.com/FeCantareliA/SGBD",
+  },
+  {
+    src: "../assets/projetos/Web-Scraping.png",
+    name: "Web Scraping",
+    conhecimentos : ["fab fa-python"],
+    href:"https://github.com/FeCantareliA/APS-Web-scraping/tree/main",
   },
   
 ];
